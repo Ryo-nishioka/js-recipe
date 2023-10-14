fa7b9b785ab057bd912f4d863369310ce7ffdf04
Ryo-nishioka <nishioka.r1001@gmail.com>
2023-10-14 16:51:22 +0900

    [カウンタ-発展1]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,5 +1,7 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const mButton = document.getElementById("m-button")
+const bButton = document.getElementById("2-button")
 
 let count = 0
 
@@ -7,6 +9,20 @@ let count = 0
 plusButton.onclick = function () {
   // count を更新
   count += 1
+  // count を表示
+  display.textContent = count
+}
+
+mButton.onclick = function () {
+  // count を更新
+  count -= 1
+  // count を表示
+  display.textContent = count
+}
+
+bButton.onclick = function () {
+  // count を更新
+  count *= 2
   // count を表示
   display.textContent = count
 }
