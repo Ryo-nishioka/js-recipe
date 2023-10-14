df528ef1fc51f77a46884d1045005d11c3741939
Ryo-nishioka <nishioka.r1001@gmail.com>
2023-10-14 14:59:38 +0900

    [3の倍数発展問題2]を学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -9,4 +9,14 @@ const genkiFunction = function (number) {
     }
   }
 }
-genkiFunction(100)
+const sanfunction = function (number) {
+  // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
+  for (let n = 1; n <= number; n++) {
+    if (n % 3 === 0 || n % 10 === 3 || (1 <= n - 30 && 9 >= n - 30)) {
+      console.log(`${n}!!!!!!!`)
+    } else {
+      console.log(n)
+    }
+  }
+}
+sanfunction(100)
