77122472fe5fd575a246fe535491515fb9b4249d
Ryo-nishioka <nishioka.r1001@gmail.com>
2023-10-15 12:09:38 +0900

    [□と○]を学習しました。

---
+++ 02_レシピ/01_□と○/main.js
@@ -0,0 +1,6 @@
+const figure = document.getElementById("figure")
+const squareButton = document.getElementById("square-button")
+const circleButton = document.getElementById("circle-button")
+figure.onclick = function () {
+  figure.classList.toggle("rounded")
+}
